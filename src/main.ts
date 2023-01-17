7f6f9390e3dfc414c73e9be878b4b46b43b59f3d
GG <zjjhgzh@gmail.com>
2023-01-17 15:27:31 +0800

    add sessionStorage

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 
 import App from "./App.vue";
 import router from "./router";
@@ -14,7 +15,10 @@ import "quasar/src/css/index.sass";
 
 const app = createApp(App);
 
-app.use(createPinia());
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
+
+app.use(pinia);
 app.use(router);
 
 app.use(Quasar, {
